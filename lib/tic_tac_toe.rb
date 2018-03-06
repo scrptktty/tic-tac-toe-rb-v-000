aaf11a623d06704e24dee339d83dfa7bebd22b75
Kachel Wittig <kjwittig@gmail.com>
2018-03-06 20:43:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,8 +38,9 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if valid_move?(board, index) == true
+  if valid_move?(board, index) 
     move(board, index)
+    display_board(board)
   else
     turn(board)
   end
