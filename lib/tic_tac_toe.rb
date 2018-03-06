b170843836ac94a94b31ba5c1ef9c8f066bc6c81
Kachel Wittig <kjwittig@gmail.com>
2018-03-06 20:42:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,12 +38,12 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if valid_move?(board, index)
+  if valid_move?(board, index) == true
     move(board, index)
-    display_board(board)
   else
     turn(board)
   end
+  display_board(board)
 end
 
 def turn_count(board)
@@ -99,11 +99,10 @@ def winner(board)
 end
 
 def play(board)
-  
+
     if over?(board) == true
       puts "Congratulations" + winner(board)
     else
       puts "Cat's Game!"
   end
 end
-
